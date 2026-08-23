faf9d7cb386ad18bd6a2b2fb9e231cd2967aa165
harshilbasketball <harshilbasketball0531@gmail.com>
2026-08-24 00:54:14 +0530

    Q2

---
+++ CLASS_QUESTIONS/Q2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.err.println("Enter the Length of side :  ");
+        int n = input.nextInt();
+        for(int i = 1;i<=n;i++){
+            for(int j = 1 ; j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
