ebc2eb358ec10cd3b419c8ac114748578d47279b
harshilbasketball <harshilbasketball0531@gmail.com>
2026-08-23 14:38:58 +0530

    Q2

---
+++ Homework_Questions/Q2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Q2{
+    static void classifyNumber(int number){
+        if(number > 0){
+            System.out.println("Positive Number ");
+        }else if(number==0){
+            System.out.println("The Number is Zero");
+
+        }else{
+            System.out.println("The Number is Negative");
+        }
+    }
+    public static void main(String[]args){
+        try(Scanner input = new Scanner(System.in)){
+        int number = input.nextInt();
+        classifyNumber(number);
+
+    }}
+}
